0d7f426a337b083f6fdf70979ba377f12a4e8ff5
amstephens132 <32180885+amstephens132@users.noreply.github.com>
2023-12-04 21:22:45 -0500

    Add files via upload

---
+++ DrivingThingy.ino
@@ -0,0 +1,22 @@
+int Switch = 1;
+
+void setup(){
+  pinMode (Switch, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop(){
+  
+  digitalWrite(Switch, LOW);
+  delay(60000);
+  if (Switch == LOW){
+    Serial.print("Switch is OFF");
+  }
+
+  digitalWrite(Switch, HIGH);
+  delay(5000);
+  if (Switch == HIGH){
+    Serial.print("Switch is ON");
+  }
+}
+
