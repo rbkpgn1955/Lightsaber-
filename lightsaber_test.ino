751149652411d9ea12bd287eb898d83e8b8352f1
amstephens132 <32180885+amstephens132@users.noreply.github.com>
2023-12-04 21:11:56 -0500

    Add files via upload

--- lightsaber_test.ino
+++ lightsaber_test.ino
@@ -1,19 +1,104 @@
 #include <FastLED.h>
-#define data 2
-#define num 1
+#define numLED 30     // 100 leds/meter
+#define pinLED 2      // pin D2
+#define lumLED 225    // STRIP BRIGHTNESS [0 - 255]
+#define satLED 255    // SATURATION [pale (0) - full (255)]
+CRGB leds[numLED];
+int hueLED;
 
-CRGB leds[num];
+// hue colors
+#define RED 0
+#define ORA 32
+#define YEL 64
+#define GRE 96
+#define AQU 128
+#define BLU 160
+#define PUR 192
+#define PIN 224
+
+// switches
+#define pinBUT 4      // pin D4 pulldown button
+int colorState = 0;   // switch counter
+int button = 0;
+
+// GY521 module
+#define pinSCL A5     // pin A5 SCL
+#define pinSDA A6     // pin A6 SDA
 
 void setup() {
-  FastLED.addLeds<WS2812B, data>(leds, num);
-  
+  delay(2000);        // delay when uploading and disconnecting
+  Serial.begin(9600); // before leds draw lots of current
+
+  FastLED.addLeds<WS2812B, pinLED, GRB>(leds, numLED);
+  FastLED.setBrightness(lumLED);
+  hueLED = RED;
+  colorCycle(hueLED);
+
+  pinMode(pinBUT, INPUT);
 }
 
 void loop() {
-  leds[0] = CRGB::White;
+
+  // button pressing
+  if(colorState > 4){ colorState = 0; } // reset color counter
+  button = digitalRead(pinBUT);
+  if(button == HIGH){
+    colorState += 1;
+  }
+
+  // change saber color bc button pressed
+  switch(colorState){
+    case 0:
+      hueLED = RED;
+      break;
+    case 1:
+      hueLED = BLU;
+      break;
+    case 2:
+      hueLED = PUR;
+      break;
+    case 3:
+      hueLED = GRE;
+      break;
+    case 4:
+      hueLED = YEL;
+      break;
+  }
+  colorCycle(hueLED);     // light that shit up
+
+  //do the test things
+  flicker(4);
+  flicker(6);
+  flicker(14);
+  flicker(16);
+  
   FastLED.show();
-  delay(100);
-  leds[0] = CRGB::Black;
+  FastLED.delay(750);     // doesn't need to flicker all the damn time
+}
+
+
+// change dem colorss
+void colorCycle(int hue){
+  for (int k=0; k<numLED; ++k){
+    leds[k] = CHSV(hue, satLED, lumLED);
+  }
   FastLED.show();
-  delay(500);
+}
+
+
+// flickering lighting effects
+// aka brightness control
+
+// test - flicker the leds before and after a given index
+void flicker (byte numled){
+  CHSV statics0 = CHSV(hueLED, satLED, lumLED-100);
+  CHSV statics1 = CHSV(hueLED, satLED, lumLED+30);
+  leds[numled-1]=statics0;
+  leds[numled+1]=statics1;
+  FastLED.show();
+  FastLED.delay(80);
+  leds[numled+1]=statics0;
+  leds[numled-1]=statics1;
+  FastLED.show();
+  FastLED.delay(50);
 }
